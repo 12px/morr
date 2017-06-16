16be8d07948314488d40f33e33abbab7dd99e84d
12px <dalton@12px.io>
2017-06-16 17:09:49 -0500

    Add initial JS library.

---
+++ src/js/morr.js
@@ -0,0 +1,109 @@
+/* morr.js */
+/* manage dom classes, no more no less */
+
+const morr = (() => {
+  'use strict';
+
+  // constructor
+  function morr(selector) {}
+
+  // library
+  morr.prototype = {
+
+    // non-chaining functions
+    do: function process(cb) {
+      // check if element exists,
+      // and process cb if state permits
+      if (this.el && this.go != false) {
+        Array.prototype.forEach.call(this.el, cb);
+      }
+      // reset state after step
+      this.go = null;
+    },
+
+    has: function hasClass(str) {
+      // check if el has class str
+      let result = false;
+      let regtst = new RegExp('(^| )' + str + '( |$)', 'gi');
+
+      this.do((el) => {
+        if (el.classList && el.classList.contains(str)) result = true;
+        if (!el.classList && regtst.test(el.className)) result = true;
+      });
+
+      return result;
+    },
+
+    // Public (chaining) functions
+    is: function isClass(str) {
+      this.go = this.has(str);
+      return this;
+    },
+
+    isnt: function isntClass(str) {
+      this.go = !this.has(str);
+      return this;
+    },
+
+    holds: function hasChild(str) {
+      // check if el has child sel = str
+      this.go = this.el.querySelector(str) != null;
+      return this;
+    },
+
+    add: function addClass(str) {
+      let cl = str.split(' ');
+
+      this.do((el) => {
+        cl.forEach((name) => {
+          if (el.classList) el.classList.add(cl[name]);
+          else el.className += ' ' + cl[name];
+        });
+      });
+
+      return this;
+    },
+
+    remove: function removeClass(str) {
+      let cl = str.split(' ').join('|');
+      let rg = new RegExp('(^|\\b)' + cl + '(\\b|$)', 'gi');
+
+      this.do((el) => {
+        if (el.classList) el.classList.remove(cl);
+        else el.className = el.className.replace(rg, ' ');
+      });
+
+      return this;
+    },
+
+    toggle: function toggleClass(str) {
+      if (this.has(str)) this.remove(str);
+      else this.add(str);
+
+      return this;
+    },
+
+    delete: function deleteElement(cb) {
+      this.do((el) => { el.parentNode.removeChild(el); });
+      if (cb) cb();
+
+      return this;
+    },
+
+    then: function thenYesOrNo(yes, no) {
+      // if true, do yes, else do no
+      if (this.go != false) yes(this.el);
+      if (this.go == false && no) no(this.el);
+      this.go = null;
+
+      return this;
+    }
+  };
+
+  return morr;
+
+})();
+
+const rr = function domorr(selector) {
+  return new morr(selector);
+};
